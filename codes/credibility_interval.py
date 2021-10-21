eca46e765d3f350e86c03b06ac7a46483a253d9a
Jose Vines <jose.vines@ug.uchile.cl>
2021-10-20 21:40:10 -0300

    Added axis option. Changed sp to np, added missing import.

--- codes/credibility_interval.py
+++ codes/credibility_interval.py
@@ -2,34 +2,38 @@
 
 Author: Jose Vines
 """
-import scipy as sp
+import numpy as np
+from scipy.special import erf
 
 
-def credibility_interval(post, alpha=1.):
+def credibility_interval(post, alpha=1., axis=None):
     """Calculate bayesian credibility interval.
 
     Parameters:
     -----------
-    post : array_like
+    post: array_like
         The posterior sample over which to calculate the bayesian credibility
         interval.
-    alpha : float, optional
+    alpha: float, optional
         Confidence level.
+    axis: int, optional
+        Along which axis will the CI be calculated.
+
     Returns:
     --------
-    med : float
+    med: float
         Median of the posterior.
-    low : float
+    low: float
         Lower part of the credibility interval.
-    up : float
+    up: float
         Upper part of the credibility interval.
 
     """
-    z = erf(alpha / sp.sqrt(2))
+    z = erf(alpha / np.sqrt(2))
 
     lower_percentile = 100 * (1 - z) / 2
     upper_percentile = 100 * (1 + z) / 2
-    low, med, up = sp.percentile(
-        post, [lower_percentile, 50, upper_percentile]
+    low, med, up = np.percentile(
+        post, [lower_percentile, 50, upper_percentile], axis=axis
     )
     return med, low, up
